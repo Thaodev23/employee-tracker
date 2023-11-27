d3041be9e3e1c22da2a510b5fee255454389b7b9
Joey Thao <thaojoey5@gmail.com>
2023-11-27 17:35:54 -0600

    update node_modules file

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
+const inquirer = require('inquirer');
 const express = require('express');
-const mysql = require('mysql2');
-require('.dotenv').config();
-const PORT = process.env.PORT || 3001;
 const app = express();
+const db = require('./server')
+app.use(express.urlencoded([{extended: false}]))
+app.use(express.json())
+const mainMenu = [{type: "list", name: "choice", message: "Please choose one", choices: ["view all departments", "view all roles", "view all employee", "add a department", "add a role", "add an employee", "update an employee role"],}]
 
-const db = mysql.createConnection(
-    {host:'localhost', 
-     user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_NAME},
-     console.log("database employee_tracker is connected")
-);
+const start = async() => {
+    await inquirer.prompt(mainMenu).then(data => {console.log(data.choice)})
+}
 
-app.listen(PORT, () => {console.log(`server is running on ${PORT}`)})
+start()
